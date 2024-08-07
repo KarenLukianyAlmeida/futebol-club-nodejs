dbced57c9ac3b3e54a1f2feddea62dc8d54643e9
Karen Almeida <karenalmeida.lukiany@gmail.com>
2024-08-07 21:58:45 +0100

    Feat: fixed model req. 13

--- app/backend/src/database/migrations/03-create-matches-table.ts
+++ app/backend/src/database/migrations/03-create-matches-table.ts
@@ -13,22 +13,27 @@ export default {
       homeTeamId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'home_team_id',
       },
       homeTeamGoals: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'home_team_goals',
       },
       awayTeamId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'away_team_id',
       },
       awayTeamGoals: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'away_team_goals',
       },
       inProgress: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'in_progress',
       },
     })
   },
